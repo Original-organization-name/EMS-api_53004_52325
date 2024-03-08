4dad0cdaac09d2ebc879fa690fa5ef491035375a
Вова Якорев <vovanyakorev2@gmail.com>
2024-03-08 20:34:59 +0100

    feat(data.employee): add employee birthdate

--- Domain/EMS.Data/Employee/Employee.cs
+++ Domain/EMS.Data/Employee/Employee.cs
@@ -7,6 +7,7 @@ public class Employee : Entity
 {
     public string Name { get; set; } = null!;
     public string Surname { get; set; } = null!;
+    public DateTime? Birthdate { get; set; }
     public string? Pesel { get; set; } 
     public Address? Address { get; set; }
     public List<Contact> Contacts { get; set; } = new();
