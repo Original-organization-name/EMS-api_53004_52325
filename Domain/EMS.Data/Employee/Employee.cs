46a85bd6423025e1fcdf18d00997b3d38687e174
Вова Якорев <vovanyakorev2@gmail.com>
2024-03-16 11:02:59 +0100

    feat(database): init database context

--- Domain/EMS.Data/Employee/Employee.cs
+++ Domain/EMS.Data/Employee/Employee.cs
@@ -8,8 +8,6 @@ public class Employee : Entity
     public string Name { get; set; } = null!;
     public string Surname { get; set; } = null!;
     public string? Pesel { get; set; } 
-    public Address? Address { get; set; }
-    public List<Contact> Contacts { get; set; } = new();
 
     public Employee(string name, string surname)
     {
---
+++ Infrastructure/EMS.PersistenceLayer/DatabaseContext.cs
@@ -0,0 +1,12 @@
+﻿using EMS.Data.Employee;
+using Microsoft.EntityFrameworkCore;
+
+namespace EMS.PersistenceLayer;
+
+public class DatabaseContext : DbContext
+{
+    public DbSet<Employee> Employees { get; set; }
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseNpgsql("Host=localhost;Database=ems_test;Username=admin;Password=V22{5ntqm3X");
+}
