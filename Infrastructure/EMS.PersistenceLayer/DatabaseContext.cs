0d0e2f26bb9812a4e75c96acb3ccb1a628ed96e7
Yakoreva Sofjia <jakorevasofija16@gmail.com>
2024-05-28 20:13:53 +0200

    feat: contracts add migrations & data seeder

--- Infrastructure/EMS.PersistenceLayer/DatabaseContext.cs
+++ Infrastructure/EMS.PersistenceLayer/DatabaseContext.cs
@@ -1,4 +1,5 @@
-﻿using EMS.Data.Dictionaries;
+﻿using EMS.Data.Contracts;
+using EMS.Data.Dictionaries;
 using EMS.Data.Education;
 using EMS.Data.Employees;
 using EMS.Data.Employees.Entities;
@@ -10,17 +11,26 @@ namespace EMS.PersistenceLayer;
 
 public class DatabaseContext : DbContext
 {
+    // Personal data
     public DbSet<Employee> Employees { get; set; } = null!;
     public DbSet<Address> Addresses { get; set; } = null!;
     public DbSet<PaymentMethod> PaymentMethods { get; set; } = null!;
     
+    // Records
     public DbSet<MedicalExamination> MedicalExaminations { get; set; } = null!;
     public DbSet<Training> Trainings { get; set; } = null!;
     public DbSet<Qualification> Qualifications { get; set; } = null!;
+
+    // Cotracts
+    public DbSet<Contract> Contracts { get; set; } = null!;
     
+    // Dictionaries
     public DbSet<QualificationItem> QualificationDict { get; set; } = null!;
     public DbSet<MedicalExamItem> MedicalExamDict { get; set; } = null!;
     public DbSet<TrainingItem> TrainingDict { get; set; } = null!;
+    public DbSet<PositionItem> PositionDict { get; set; } = null!;
+    public DbSet<WorkplaceItem> WorkplaceDict { get; set; } = null!;
+    public DbSet<OccupationCodeItem> OccupationCodeDict { get; set; } = null!;
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
@@ -32,5 +42,8 @@ public class DatabaseContext : DbContext
     {
         modelBuilder.Entity<Education>()
             .OwnsOne(p => p.Period);
+        
+        modelBuilder.Entity<OccupationCodeItem>()
+            .HasKey(p => p.Code);
     }
 }
