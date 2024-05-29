683eb06cbbef4887da5ecd51ddcf94c2c1dfa6ff
Вова Якорев <vovanyakorev2@gmail.com>
2024-05-29 17:23:53 +0200

    feat: add contract on employee create

--- Presentation/EMS.Presentation/RequestModels/CreateEmployeeModel.cs
+++ Presentation/EMS.Presentation/RequestModels/CreateEmployeeModel.cs
@@ -1,9 +1,11 @@
-﻿using EMS.DTO.Employee;
+﻿using EMS.DTO.Contracts;
+using EMS.DTO.Employee;
 using EMS.DTO.Records;
 
 namespace EMS.Presentation.RequestModels;
 
 public record CreateEmployeeModel(
     EmployeeDto Employee,
+    ContractDto? Contract,
     List<MedicalExaminationDto>? MedicalExaminations,
     List<TrainingDto>? Trainings);
